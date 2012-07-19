6c7a54f760b292410d7915cbf0692ad4b7977dda
Hiromitsu FUJITA <bow.fujita@gmail.com>
2012-07-19 00:55:01 -0700

    ejsでビュースクリプトを書く

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/mytemplate/:p', routes.mytemplate);
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.mytemplate = function(req, res){
+  res.render('mytemplate', {
+      title: 'My Template'
+    , param1: req.params.p
+    , param2: 'hard coded'
+  });
+};
